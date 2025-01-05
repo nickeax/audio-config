5d55a507fd6bb8132ff8b079410d66e076a698ff
Nick Fletcher <nickeax@gmail.com>
2025-01-05 11:46:03 +1100

    Initial commit

---
+++ app/models/rackItem.js
@@ -0,0 +1,9 @@
+export class RackItem {
+  constructor(id, name, description, price, quantity) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.quantity = quantity;
+  }
+}
